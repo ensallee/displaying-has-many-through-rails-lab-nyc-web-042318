85d1cb39497d568a7f5a4326ce94903449608fdf
ensallee <ensallee@gmail.com>
2018-05-22 14:19:53 -0400

    solved

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  resources :appointments, only: [:show]
+  resources :doctors
+  resources :patients
   resources :doctors
   resources :patients
   resources :appointments, only: [:show]
